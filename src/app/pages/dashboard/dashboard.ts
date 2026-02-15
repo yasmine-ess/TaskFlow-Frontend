128b089dc709e3f155923eac85e955568a0db56b
yasmine-ess <yasmine.esseghir28@gmail.com>
2026-02-15 17:50:13 +0100

    routes and dashboard added

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,  
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.css'],
+  
+
+
+})
+export class DashboardComponent {
+
+}
